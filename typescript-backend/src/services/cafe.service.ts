18a539ef4e4053c93e35c315e54acc47da1b8389
Rohan Nankani <rohan.nankani2021@gmail.com>
2025-06-11 18:43:18 -0400

    remove comments

--- typescript-backend/src/services/cafe.service.ts
+++ typescript-backend/src/services/cafe.service.ts
@@ -2,12 +2,11 @@ import { CustomModel } from '../../orm/custom';
 import { ICafe } from '../interfaces/cafe.interface';
 import { ICafeServiceAPI } from '../interfaces/cafe.service.interface'; 
 
-// Just having this here for now but maybe better to have this as ENUM
 const cafeModel = new CustomModel('Cafe');
 
 const cafeService: ICafeServiceAPI = {
   async getAllCafes(): Promise<ICafe[]> {
-    return cafeModel.findMany(); // Maybe a better way to do this?
+    return cafeModel.findMany();
   },
 
   async getCafeById(id: number): Promise<ICafe | null> {
