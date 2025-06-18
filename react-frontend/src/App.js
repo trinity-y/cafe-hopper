d26d8bdb8718777f830fdfbca6147f404a225479
safiyamak <smakada@uwaterloo.ca>
2025-06-18 10:54:36 -0400

    Adding cafe search component
    This will be used to search for cafes. It currently has the buttons disabled (will be implemented later)
    But you can search from the existing list of cafes.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
+import CafeSearchPage from './pages/CafeSearch';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<LoginPage/>} />
           <Route path='/signup' element={<SignupPage/>} />
+          <Route path='/cafesearch' element={<CafeSearchPage/>} />
         </Routes>
     </Router>
   );
