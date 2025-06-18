9f37f3861ca57a4ff29fb3da6418828442b71f87
Trinity <32817635+trinity-y@users.noreply.github.com>
2025-06-17 21:24:57 -0400

    revert server.ts as per discussion in gc

--- typescript-backend/src/server.ts
+++ typescript-backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+const PORT: number = 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
