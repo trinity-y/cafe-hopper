cac21798bec9ce55fea5b4b5b0766f1d6ad6b19e
safiyamak <smakada@uwaterloo.ca>
2025-07-21 00:57:05 -0400

    Update routing to api call

--- react-frontend/src/components/CreateReview.jsx
+++ react-frontend/src/components/CreateReview.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import {Box, TextField, Grid, Button, Alert} from '@mui/material';
 import theme from './theme';
 import StarRating from './StarRating';
-import CreateReviewRow from './CreateReviewRow';
+import CreateReviewRow from '../api/CreateReviewRow';
 import { getAuth } from "firebase/auth";
 
 function CreateReview({cafeName, cid}) {
