e8585d4247e83dfa19fdea87782b6d564c4b05cb
celina-he <celinahe33@gmail.com>
2025-07-24 01:57:42 -0400

    remove backendClient

--- react-frontend/src/api/base.js
+++ react-frontend/src/api/base.js
@@ -6,8 +6,4 @@ const client = axios.create({
     baseURL: isLocal ? process.env.REACT_APP_LOCAL_API_URL : process.env.REACT_APP_PROD_API_URL
 });
 
-const backendClient = axios.create({
-    baseURL: isLocal ? process.env.REACT_APP_LOCAL_BACKEND_URL : process.env.REACT_APP_PROD_BACKEND_URL
-});
-
-export { client, backendClient };
+export { client };
