11a3a793de2930f0742cf77c4252c6f74577f400
trinity-y <tayip@uwaterloo.ca>
2025-07-28 17:23:32 -0400

    feed sql

---
+++ sql-tests/feed-test-sample.sql
@@ -0,0 +1,29 @@
+-- create the view of reviews for user 87
+CREATE OR REPLACE VIEW "reviewsAndLikesForUser87" AS
+SELECT "rid", "overallRating", "foodRating", "drinkRating", "atmosphereRating", "notes", "timestamp", "author", "cid", COALESCE("likes", 0) AS likes
+FROM
+(SELECT 
+    "Reviews"."id" AS "rid",
+    "Reviews"."rating" AS "overallRating",
+    "Reviews"."drinkrating" AS "drinkRating",
+    "Reviews"."foodrating" AS "foodRating",
+    "Reviews"."atmosphererating" AS "atmosphereRating",
+    "Reviews"."notes" AS "notes",
+    "Reviews"."timestamp" AS "timestamp",
+    "Reviews"."uid" AS "author",
+    "Reviews"."cid" AS "cid",
+    "Friend"."user_id" AS "uid"
+FROM "Reviews" INNER JOIN "Friend" ON "Reviews"."uid" = "Friend"."following_id"
+WHERE "Friend"."user_id" = 87
+LIMIT 50 ) AS "friendReviews" LEFT JOIN
+( SELECT COUNT(*) AS likes, rid AS likerid
+FROM "Reaction" WHERE "reaction" = 'like'
+GROUP BY "rid" ) AS "likeCounts"
+ON "friendReviews"."rid" = "likeCounts"."likerid";
+
+-- get info needed for frontend
+SELECT "User"."username", "User"."id" AS uid, "Cafe"."name", "rid", "overallRating", "drinkRating", "foodRating", "atmosphereRating", "notes", "timestamp", "likes"
+FROM "reviewsAndLikesForUser87", "User", "Cafe"
+WHERE "reviewsAndLikesForUser87"."author" = "User"."id" AND
+    "reviewsAndLikesForUser87"."cid" = "Cafe"."id"
+ORDER BY "timestamp" DESC;
